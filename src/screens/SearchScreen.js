ece8031b22173fb8a0ff12b8af17c33307389619
jyoti <jyotykumari63@gmail.com>
2024-12-21 12:20:03 +0530

    adding navigation

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+const SearchScreen = () => {
+  return (
+    <View>
+      <Text>Search Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  viewStyle: {},
+});
+export default SearchScreen;
